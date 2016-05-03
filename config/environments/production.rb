39873938e62da431bad5d4de202a94bd442699e9
kirill <swollenseal@mail.ru>
2016-05-03 17:33:35 +0300

    Added sendgrid production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { host: 'https://gentle-shore-71272.herokuapp.com' }
+  config.action_mailer.delivery_mathod= :smtp
+  
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
 end
